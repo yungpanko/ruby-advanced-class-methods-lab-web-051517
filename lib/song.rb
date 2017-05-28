437f2cfd8d2e61ab52f96c136db7ca88886dba44
yungpanko <jrjohns@mit.edu>
2017-05-28 16:47:33 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,4 @@
-require 'pry'
+require "pry"
 
 class Song
   attr_accessor :name, :artist_name
@@ -13,8 +13,9 @@ class Song
   end
 
   def self.create
-    self.all << Song.new
-    self.all.last
+    song = Song.new
+    song.save
+    song
   end
 
   def self.new_by_name(name)
@@ -24,17 +25,19 @@ class Song
   end
 
   def self.create_by_name(name)
-    self.all << Song.new
-    self.all.last.name = name
-    self.all.last
+    song = self.new_by_name(name)
+    song.save
+    song
   end
 
   def self.find_by_name(name)
-    self.all.detect {|song| song.name == name}
+    self.all.find do |song|
+      song.name == name
+    end
   end
 
   def self.find_or_create_by_name(name)
-    unless self.find_by_name(name) == nil
+    if self.find_by_name(name)
       self.find_by_name(name)
     else
       self.create_by_name(name)
@@ -46,24 +49,20 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    split = filename.split(" - ")
-    split_name = split[1][0...-4]
-    split_artist = split[0]
-    song = Song.new
-    song.name = split_name
-    song.artist_name = split_artist
+    song_data = filename.split(" - ")
+    song = self.find_or_create_by_name(song_data.last.gsub(".mp3",""))
+    song.artist_name = song_data.first
     song
   end
 
   def self.create_from_filename(filename)
-    self.all << self.new_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+    song
   end
 
   def self.destroy_all
     @@all = []
   end
-
-
-
 
 end
